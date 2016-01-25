df9f772efd66d374e74a199d9bfeb7e7d20d20f0
ashokfederer <ashokfederer@gmail.com>
2016-01-25 16:53:10 +0530

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 int main(){
-     int n,shift=1,base,res=0;
+     int n,base,res[50],i=0,j,rem;
      printf("Enter the number : ");
      printf("\nEnter the base : ");
      scanf("%d%d",&n,&base);
-
     while(n>0){
-        res=res+(n%base)*shift;
-        shift=shift*10;
+        rem=n%base;
+        if(rem<10)
+        	res[i]=rem+48;
+        else
+        	res[i]=rem+55;
         n=n/base; 
+    	i++;
     }
-    printf("\nResult : %d",res); 
-         return 0;
+    printf("\nResult : ");
+    for(j=i-1;j>=0;j--)
+    {
+    	printf("%c",res[j]);
+    }
+    	return 0;
 }
